bc22fbcabec9922fc48ec2af7bc106ab98c2bd92
Gatwech Tap Nguth <gatwech3211@gmail.com>
2023-06-29 21:42:17 +0300

    UI tangibility

--- portfolio-next-js/components/Elements/Tabs.jsx
+++ portfolio-next-js/components/Elements/Tabs.jsx
@@ -32,8 +32,8 @@ const Tabs = ({ tabsData }) => {
                     href=""
                     className={
                       indx === activeTab
-                        ? "md:mx-10 font-montserrat uppercase text-sm cursor-pointer mr-6 py-3 text-base font-semiblod relative  font-medium after:content-[''] after:absolute after:w-[100%] text-[#D31717] after:transition-all hover:after:w-[100%] after:h-[2px] after:bg-[#D31717] hover:text-[#D31717] after:bottom-2 after:left-0"
-                        : "md:mx-10 font-montserrat uppercase text-sm cursor-pointer mr-6 py-3 relative text-base font-semiblod text-gray-500 dark:text-gray-20 after:content-[''] after:absolute after:w-0 after:transition-all hover:after:w-[100%] after:h-[2px] after:bg-[#D31717] hover:text-[#D31717] after:bottom-2 after:left-0"
+                        ? "md:mx-10 font-montserrat uppercase text-sm cursor-pointer mr-6 py-3 font-semiblod relative  font-medium after:content-[''] after:absolute after:w-[100%] text-[#D31717] after:transition-all hover:after:w-[100%] after:h-[2px] after:bg-[#D31717] hover:text-[#D31717] after:bottom-2 after:left-0"
+                        : "md:mx-10 font-montserrat uppercase text-sm cursor-pointer mr-6 py-3 relative font-semiblod text-gray-500 dark:text-gray-20 after:content-[''] after:absolute after:w-0 after:transition-all hover:after:w-[100%] after:h-[2px] after:bg-[#D31717] hover:text-[#D31717] after:bottom-2 after:left-0"
                     }
                   >
                     {tab.title}
