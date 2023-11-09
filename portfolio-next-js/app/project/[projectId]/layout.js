e3c50930135f438de70ae1dee72b4b7d74509121
= <gatwech3211@gmail.com>
2023-11-09 16:55:49 +0300

    Project Id in layout

--- portfolio-next-js/app/project/[projectId]/layout.js
+++ portfolio-next-js/app/project/[projectId]/layout.js
@@ -1,12 +1,7 @@
 export default function ProjectLayout({ children }) {
-   return (
-    <html lang="en">
-      <body className="bg-white dark:bg-gray-800 ">
-        <Providers>
-           {children}
-          <
-     
-      </body>
-    </html>
+  return (
+    <main lang="en">
+      <div className="bg-white dark:bg-gray-800 ">{children}</div>
+    </main>
   );
-   }
+}
